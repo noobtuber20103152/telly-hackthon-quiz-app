c47eb6ec21257bd8e07bbcd1184714d03a034440
ujjawal <ujjawalrachhoya100@gmail.com>
2022-07-17 17:19:46 +0530

    update

--- components/Homepage-Components/FrontHomePage.js
+++ components/Homepage-Components/FrontHomePage.js
@@ -35,10 +35,9 @@ function FrontHomePage() {
         <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                        <br className="hidden lg:inline-block" />readymade gluten
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcom to Quizzly
                     </h1>
-                    <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <p className="mb-8 leading-relaxed">Our mission is to create a user friendly quizz webapp where quiz admin could easily register and prepare a quiz . And using it's unique quiz code anyone in the world who is eligible to give that quiz could take up that quiz.</p>
                     <div className="flex justify-center">
                         <button onClick={createQuiz} className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Create a quiz</button>
                         <Link href="/takequiz">
@@ -47,7 +46,7 @@ function FrontHomePage() {
                     </div>
                 </div>
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img className="object-cover object-center rounded" alt="hero" src="https://previews.123rf.com/images/lineartestpilot/lineartestpilot1810/lineartestpilot181010862/110714666-cartoon-doodle-font-quiz.jpg" />
+                    <img className="object-cover object-center rounded" alt="hero" src="https://bluemetropolis.org/wp-content/uploads/2020/10/quiz.jpg" />
                 </div>
             </div>
         </section>
